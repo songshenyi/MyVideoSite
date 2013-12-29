3f85cbfc9d4c21081de09578bdfca107af116ec9
songshenyi <quickbasic411@gmail.com>
2013-12-29 11:29:16 +0800

    initial Diagnostics project

---
+++ server/Diagnostics/LogEngine/Logger/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVideoSite.Diagnostics.LogEngine
+{
+    public class Logger
+    {
+        public static void Report(string message)
+        {
+
+        }
+
+        public static void ReportFromException(Exception ex)
+        {
+
+        }
+    }
+}
---
+++ server/Encoder/EncoderLib/Encoder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Expression.Encoder;
+using Microsoft.Expression.Encoder.Profiles;
+using MyVideoSite.Diagnostics.LogEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyVideoSite.Server.EncoderLib
+{
+    public class Encoder
+    {
+        public MediaItem CreateMediaItem(string fileName)
+        {
+            MediaItem mediaItem;
+            try
+            {
+                mediaItem = new MediaItem(fileName);
+                AdvancedVC1VideoProfile profile = new AdvancedVC1VideoProfile();
+                profile.SmoothStreaming = true;
+                profile.AdaptiveGop = false;
+                mediaItem.OutputFormat.VideoProfile = profile;
+            }
+            catch (Exception ex)
+            {
+                Logger.ReportFromException(ex);
+                throw;
+            }
+            return mediaItem;
+        }
+    }
+}
