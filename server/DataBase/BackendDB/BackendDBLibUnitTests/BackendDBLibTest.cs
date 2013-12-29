9f532b45fd092e7694907c92e057e5ebb89ee318
songshenyi <quickbasic411@gmail.com>
2013-12-29 19:01:34 +0800

    add MongoDB Unit test project

---
+++ server/DataBase/BackendDB/BackendDBLibUnitTests/BackendDBLibTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyVideoSite.DataBase.BackendDBLibUnitTests
+{
+    [TestClass]
+    public class BackendDBLibTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
